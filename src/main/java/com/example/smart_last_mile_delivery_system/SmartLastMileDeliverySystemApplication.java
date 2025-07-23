b0c9fa4d0899184b8c3fccd6dcf6537bd01600fb
Lam <qwekhoilam@gmail.com>
2025-07-23 16:09:01 +0700

    Initial commit from local folder

---
+++ src/main/java/com/example/smart_last_mile_delivery_system/SmartLastMileDeliverySystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_last_mile_delivery_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLastMileDeliverySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartLastMileDeliverySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smart_last_mile_delivery_system/SmartLastMileDeliverySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_last_mile_delivery_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLastMileDeliverySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
